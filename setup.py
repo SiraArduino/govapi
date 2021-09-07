259f80fee2415786cb456319a7ea96a6e7efb82b
siraben <42887687+SiraArduino@users.noreply.github.com>
2021-09-07 19:07:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 import os.path
 
 setup(
-  name = 'GovAPI',         # How you named your package folder (MyLib)
-  packages = ['GovAPI'],   # Chose the same as "name"
+  name = 'govapi',         # How you named your package folder (MyLib)
+  packages = ['govapi'],   # Chose the same as "name"
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Show my name',   # Give a short description about your library
@@ -14,7 +14,7 @@ setup(
   download_url = 'https://github.com/SiraArduino/govapi/archive/0.0.1.zip',    # I explain this later on
   keywords = ['govapi', 'Sira', 'THAILAND', 'Benchapoowanon'],   # Keywords that define your package best
   install_requires=[
-          'siraben',
+          'govapi',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
